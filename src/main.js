cda3253e887d344f262521c41016e5248f8eaa1a
Leahpar Alrac <lal_ro@outlook.com>
2024-07-08 19:01:52 -0400

    commit inicial

---
+++ src/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudar(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(3, 4);
+const saudacao = saudar("Alice");
+console.log(resultadoMultiplicacao);
+console.log(saudacao);
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(3, 4);
+const saudacao = saudar("Alice");
+
+console.log(resultadoMultiplicacao);
+console.log(saudacao);
